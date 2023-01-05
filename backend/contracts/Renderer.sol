d762b5feaea84e96e200f8b47a34065af1f5187a
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2023-01-05 20:48:13 +0200

    Renderer WIP

--- backend/contracts/Renderer.sol
+++ backend/contracts/Renderer.sol
@@ -1,255 +1,278 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
-import "./Trigonometry.sol";
+// import "./Trigonometry.sol";
 import "./Utilities.sol";
 
 contract Renderer {
   uint256 constant SIZE = 500;
+  uint256 constant RADIUS = 200;
 
-  struct Planet {
-    uint256 planetRadius;
-    uint256 ringsOffset;
-    uint256 orbitRadius;
-    uint256[3] color;
-    uint256 initialAngleDegrees;
+  struct ColorPalette {
+    uint256 primaryHue;
+    uint256 secondaryHue;
+    utils.HSL mainColorHsl;
+    utils.HSL secondaryColorHsl;
+    utils.HSL backgroundColor;
+    utils.HSL darkerSecondaryHsl;
+    utils.HSL[] primaryColorsHsl;
+    utils.HSL[] secondaryColorsHsl;
+  }
+
+  struct Kaleidoscope {
+    uint256 tokenId;
+    uint256 repetitions; // n
+    uint256 numInsideArtifacts; // numCircles
+    uint256 numOutsideArtifacts;
+    uint256 centerX;
+    uint256 centerY;
+    bool hasGradient;
+    bool hasSecondaryColor;
+  }
+
+  struct AnimatedCircle {
+    // Start position
+    uint256 x1;
+    uint256 y1;
+    // End position
+    uint256 x2;
+    uint256 y2;
+    uint256 radius;
+    uint256 alternatingClass;
+    uint256 duration;
+    utils.HSL color;
+  }
+
+  struct AnimatedTriangle {
+    uint256 x1;
+    uint256 y1;
+    uint256 duration;
+    utils.HSL color;
+  }
+
+  struct AnimatedRectangle {
+    uint256 width;
+    uint256 height;
     uint256 duration;
   }
 
-  function translateWithAngle(
-    int256 x,
-    int256 y,
-    uint256 degrees
-  ) internal pure returns (int256, int256) {
-    int256 newX = x;
-    int256 newY = y;
+  function colorPaletteForKaleidescope(Kaleidoscope memory _kaleidoscope)
+    public
+    pure
+    returns (ColorPalette memory palette)
+  {
+    palette.primaryHue = utils.randomRange(_kaleidoscope.tokenId, "primaryHue", 0, 360);
+    palette.secondaryHue = _kaleidoscope.hasSecondaryColor ? (palette.primaryHue + 240) % 360 : palette.primaryHue;
+    palette.mainColorHsl = utils.HSL(palette.primaryHue, 100, 30);
+    palette.secondaryColorHsl = utils.HSL(palette.secondaryHue, 100, 70);
+    palette.backgroundColor = utils.HSL(palette.primaryHue, 100, 10);
+    palette.darkerSecondaryHsl = utils.HSL((palette.primaryHue + 10) % 360, 100, 10);
+    palette.primaryColorsHsl = utils.generateColors(palette.primaryHue, _kaleidoscope.numInsideArtifacts + 1);
+    palette.secondaryColorsHsl = utils.generateColors(palette.secondaryHue, _kaleidoscope.numInsideArtifacts + 1);
 
-    newX =
-      x *
-      Trigonometry.cos(degrees * (Trigonometry.PI / 180)) -
-      y *
-      Trigonometry.sin(degrees * (Trigonometry.PI / 180));
-    newY =
-      x *
-      Trigonometry.sin(degrees * (Trigonometry.PI / 180)) +
-      y *
-      Trigonometry.cos(degrees * (Trigonometry.PI / 180));
-
-    return (newX, newY);
+    return palette;
   }
 
+  function kaleidoscopeForTokenId(uint256 _tokenId) public pure returns (Kaleidoscope memory kaleidoscope) {
+    kaleidoscope.tokenId = _tokenId;
+    kaleidoscope.repetitions = utils.randomRange(_tokenId, "repetitions", 3, 6);
+    kaleidoscope.numInsideArtifacts = utils.randomRange(_tokenId, "numInsideArtifacts", 3, 10);
+    kaleidoscope.numOutsideArtifacts = utils.randomRange(_tokenId, "numOutsideArtifacts", 1, 4);
+    kaleidoscope.hasGradient = utils.randomRange(_tokenId, "hasGradient", 1, 10) == 5;
+    kaleidoscope.hasSecondaryColor = utils.randomRange(_tokenId, "secondaryColor", 1, 8) == 8;
 
-  /**
-  * @notice Gets the SVG representation of a planet's orbit.
-  * @param planet The planet to generate the SVG for.
-  */
-  function getOrbitSVG(Planet memory planet) public pure returns (string memory) {
-    uint256 halfCanvasWidth = SIZE / 2;
+    return kaleidoscope;
+  }
 
-    // Calculate the initial position of the planet
-    int256 x = int256(planet.orbitRadius);
-    int256 y = 0;
+  function circleAtIndexForKaleidescope(
+    Kaleidoscope memory _kaleidescope,
+    ColorPalette memory _palette,
+    uint256 _index
+  ) public pure returns (AnimatedCircle memory circle) {
+    circle.x1 = utils.randomRange(
+      _kaleidescope.tokenId,
+      string.concat("x1", utils.uint2str(_index)),
+      0,
+      _kaleidescope.centerX
+    );
+    circle.x2 = utils.randomRange(
+      _kaleidescope.tokenId,
+      string.concat("x2", utils.uint2str(_index)),
+      circle.radius / 2,
+      _kaleidescope.centerX
+    );
+    circle.y1 = utils.randomRange(
+      _kaleidescope.tokenId,
+      string.concat("y1", utils.uint2str(_index)),
+      0,
+      _kaleidescope.centerY
+    );
+    circle.y2 = utils.randomRange(
+      _kaleidescope.tokenId,
+      string.concat("y2", utils.uint2str(_index)),
+      _kaleidescope.centerY,
+      RADIUS
+    );
+    circle.radius = utils.randomRange(
+      _kaleidescope.tokenId,
+      string.concat("radius", utils.uint2str(_index)),
+      5,
+      RADIUS / 6
+    );
+    circle.duration = utils.randomRange(
+      _kaleidescope.tokenId,
+      string.concat("duration", utils.uint2str(_index)),
+      5,
+      10
+    );
+    circle.alternatingClass = utils.randomRange(
+      _kaleidescope.tokenId,
+      string.concat("alternatingClass", utils.uint2str(_index)),
+      0,
+      3
+    );
+    circle.color = _index % 2 == 0 ? _palette.primaryColorsHsl[_index] : _palette.secondaryColorsHsl[_index];
 
-    (int256 innerX, int256 innerY) = translateWithAngle(x - 1, y, planet.initialAngleDegrees);
-    (int256 outerX, int256 outerY) = translateWithAngle(x, y, planet.initialAngleDegrees);
+    return circle;
+  }
 
-    string memory colorTuple = string.concat(
-      utils.uint2str(planet.color[0]),
-      ",",
-      utils.uint2str(planet.color[1]),
-      ",",
-      utils.uint2str(planet.color[2])
+  function rectangleAtIndexForKaleidescope(Kaleidoscope memory _kaleidescope, uint256 _index)
+    public
+    pure
+    returns (AnimatedRectangle memory rectangle)
+  {
+    rectangle.width = utils.randomRange(
+      _kaleidescope.tokenId,
+      string.concat("width", utils.uint2str(_index)),
+      RADIUS / 2,
+      RADIUS
+    );
+    rectangle.height = utils.randomRange(
+      _kaleidescope.tokenId,
+      string.concat("height", utils.uint2str(_index)),
+      RADIUS / 2,
+      RADIUS
+    );
+    rectangle.duration = utils.randomRange(
+      _kaleidescope.tokenId,
+      string.concat("duration", utils.uint2str(_index)),
+      5,
+      10
     );
 
-    // Generate the SVG string
-    string memory renderedSVG = string.concat(
-      '<circle cx="',
-      utils.uint2str(halfCanvasWidth),
-      '" cy="',
-      utils.uint2str(halfCanvasWidth),
-      '" r="',
-       utils.uint2str(planet.orbitRadius),
-      '" fill="none" stroke="rgba(',
-      colorTuple,
-      ',0.5)"/>',
-      // Inner circle
-      '<g><circle cx="',
-      utils.uint2str(uint256(int256(halfCanvasWidth) + innerX / 1e18)),
-      '" cy="'
-      
+    return rectangle;
+  }
+
+  function triangleAtIndexForKaleidescope(Kaleidoscope memory _kaleidescope, uint256 _index)
+    public
+    pure
+    returns (AnimatedTriangle memory triangle)
+  {
+    triangle.x1 = utils.randomRange(
+      _kaleidescope.tokenId,
+      string.concat("x1", utils.uint2str(_index)),
+      0,
+      _kaleidescope.centerX
+    );
+    triangle.y1 = utils.randomRange(
+      _kaleidescope.tokenId,
+      string.concat("y1", utils.uint2str(_index)),
+      0,
+      _kaleidescope.centerY
+    );
+    triangle.duration = utils.randomRange(
+      _kaleidescope.tokenId,
+      string.concat("duration", utils.uint2str(_index)),
+      5,
+      10
     );
 
-    renderedSVG = string.concat(
-      renderedSVG,
-      utils.uint2str(uint256(int256(halfCanvasWidth) - innerY / 1e18)),
-      '" r="',
-      utils.uint2str(planet.planetRadius - 2),
-      '" fill="rgb(',
-      colorTuple,
-      ')"/>'
-      // Outer circle
-      '<circle cx="',
-      utils.uint2str(uint256(int256(halfCanvasWidth) + outerX / 1e18)),
-      '" cy="'
-    );
+    return triangle;
+  }
 
-    renderedSVG = string.concat(
-      renderedSVG,
-      utils.uint2str(uint256(int256(halfCanvasWidth) - outerY / 1e18)),
-      '" r="',
-      utils.uint2str(planet.planetRadius),
-      '" fill-opacity="0.8" fill="rgb(',
-      colorTuple,
-      ')"/>'
-    );
+  function getSVGForCircle(AnimatedCircle memory _circle) public pure returns (string memory) {
+    string memory animations = "";
 
-    if (planet.ringsOffset != 0) {
-      uint256 ringsRadius = planet.planetRadius + planet.ringsOffset;
-      renderedSVG = string.concat(
-        renderedSVG,
-        // Rings
+    if (_circle.alternatingClass >= 2) {
+      animations = _circle.alternatingClass == 3
+        ? string.concat(
+          '<animate attributeName="cx" values="',
+          utils.uint2str(_circle.x1),
+          ";",
+          utils.uint2str(_circle.x2),
+          ";",
+          utils.uint2str(_circle.x1),
+          '" keyTimes="0;0.5;1" calcMode="linear" begin="0s" dur="',
+          utils.uint2str(_circle.duration),
+          's" repeatCount="indefinite"/>'
+        )
+        : string.concat(
+          '<animate attributeName="cy" values="',
+          utils.uint2str(_circle.y1),
+          ";",
+          utils.uint2str(_circle.y2),
+          ";",
+          utils.uint2str(_circle.y1),
+          '" keyTimes="0;0.5;1" calcMode="linear" begin="0s" dur="',
+          utils.uint2str(_circle.duration / 2),
+          's" repeatCount="indefinite"/>'
+        );
+    } else {
+      animations = string.concat(
+        '<animate attributeName="cx" values="',
+        utils.uint2str(_circle.x1),
+        ";",
+        utils.uint2str(_circle.x2),
+        ";",
+        utils.uint2str(_circle.x1),
+        '" keyTimes="0;0.5;1" calcMode="linear" begin="0s" dur="',
+        utils.uint2str(_circle.duration),
+        's" repeatCount="indefinite"/>',
+        '<animate attributeName="cy" values="',
+        utils.uint2str(_circle.y1),
+        ";",
+        utils.uint2str(_circle.y2),
+        ";",
+        utils.uint2str(_circle.y1),
+        '" keyTimes="0;0.5;1" calcMode="linear" begin="0s" dur="',
+        utils.uint2str(_circle.duration / 2),
+        's" repeatCount="indefinite"/>'
+      );
+    }
+
+    return
+      string.concat(
         '<circle cx="',
-        utils.uint2str(uint256(int256(halfCanvasWidth) + outerX / 1e18)),
+        utils.uint2str(_circle.radius / 2),
         '" cy="',
-        utils.uint2str(uint256(int256(halfCanvasWidth) - outerY / 1e18)),
+        utils.uint2str(_circle.radius / 2),
         '" r="',
-        utils.uint2str(ringsRadius),
-        '" fill="none" stroke-width="1" stroke="rgb(',
-        colorTuple,
-        ')"/>'
+        utils.uint2str(_circle.radius),
+        '" fill="',
+        "black",
+        '">',
+        animations,
+        "</circle>"
       );
-    }
-
-    renderedSVG = string.concat(
-      renderedSVG,
-      '<animateTransform attributeName="transform" type="rotate" from="0 ',
-      utils.uint2str(halfCanvasWidth),
-      " ",
-      utils.uint2str(halfCanvasWidth),
-      '" to="360 ',
-      utils.uint2str(halfCanvasWidth),
-      " ",
-      utils.uint2str(halfCanvasWidth),
-      '" dur="'
-    );
-
-    renderedSVG = string.concat(
-      renderedSVG,
-      utils.uint2str(planet.duration),
-      's" repeatCount="indefinite"></animateTransform>',
-      "</g>"
-    );
-
-    return renderedSVG;
-  }
-
-  /**
-  * @notice Gets the number of planets in a solar system.
-  * @param _tokenId The token ID of the solar system to get the number of planets for.
-  */
-  function numPlanetsForTokenId(uint256 _tokenId) public pure returns (uint256) {
-    return utils.randomRange(_tokenId, "numPlanets", 1, 6);
-  }
-
-  /**
-  * @notice Gets the number of ringed planets in a solar system.
-  * @param _tokenId The token ID of the solar system to get the number of ringed planets for.
-  */
-  function numRingedPlanetsForTokenId(uint256 _tokenId) public pure returns (uint256) {
-    uint256 numRingedPlanets;
-    for (uint256 i = 0; i < numPlanetsForTokenId(_tokenId); i++) {
-      if (utils.randomRange(_tokenId, string.concat("ringsOffset", utils.uint2str(i)), 0, 10) == 5) {
-        numRingedPlanets++;
-      }
-    }
-    return numRingedPlanets;
-  }
-
-  /**
-  * @notice Determines if a solar system has a rare star.
-  * @param _tokenId The token ID of the solar system to check.
-  */
-  function hasRareStarForTokenId(uint256 _tokenId) public pure returns (bool) {
-    return utils.randomRange(_tokenId, "rareStar", 0, 10) == 5;
-  }
-
-  /**
-  * @notice Gets the SVG representation of a solar system.
-  * @param _tokenId The token ID of the solar system to generate the SVG for.
-  */
-  function getSVG(uint256 _tokenId) public pure returns (string memory) {
-    uint256 numPlanets = numPlanetsForTokenId(_tokenId);
-    uint256 radiusInterval = SIZE / 2 / (numPlanets + 3);
-    uint256 planetRadiusUpperBound = utils.min(radiusInterval / 2, SIZE / 4);
-    uint256 planetRadiusLowerBound = radiusInterval / 4;
-
-    uint256 starRadius = utils.randomRange(_tokenId, "starRadius", radiusInterval, radiusInterval * 2 - 10);
-    string memory starAttributes = hasRareStarForTokenId(_tokenId) ? 'fill="#39B1FF"' : 'fill="#FFDA17"';
-
-    string memory renderSvg = string.concat(
-      '<svg width="',
-      utils.uint2str(SIZE),
-      '" height="',
-      utils.uint2str(SIZE),
-      '" viewBox="0 0 ',
-      utils.uint2str(SIZE),
-      " ",
-      utils.uint2str(SIZE),
-      '" xmlns="http://www.w3.org/2000/svg">',
-      '<rect width="',
-      utils.uint2str(SIZE),
-      '" height="',
-      utils.uint2str(SIZE),
-      '" fill="#0D1F2F"></rect>',
-      '<circle cx="',
-      utils.uint2str(SIZE / 2),
-      '" cy="',
-      utils.uint2str(SIZE / 2),
-      '" r="',
-      utils.uint2str(starRadius),
-      '" ',
-      starAttributes,
-      "/>"
-    );
-
-    for (uint256 i = 0; i < numPlanets; i++) {
-      Planet memory planet;
-
-      if (utils.randomRange(_tokenId, string.concat("ringsOffset", utils.uint2str(i)), 0, 10) == 5) {
-        planet.ringsOffset = 4;
-      }
-
-      planet.planetRadius = utils.randomRange(
-        _tokenId,
-        string.concat("planetRadius", utils.uint2str(i)),
-        planetRadiusLowerBound,
-        planetRadiusUpperBound - planet.ringsOffset
-      );
-
-      planet.orbitRadius = radiusInterval * (i + 3);
-      planet.duration = utils.randomRange(_tokenId, string.concat("duration", utils.uint2str(i)), 5, 15);
-
-      planet.color[0] = utils.randomRange(_tokenId, string.concat("colorR", utils.uint2str(i)), 100, 255);
-      planet.color[1] = utils.randomRange(_tokenId, string.concat("colorG", utils.uint2str(i)), 100, 255);
-      planet.color[2] = utils.randomRange(_tokenId, string.concat("colorB", utils.uint2str(i)), 100, 255);
-
-      planet.initialAngleDegrees = utils.randomRange(
-        _tokenId,
-        string.concat("initialAngle", utils.uint2str(i)),
-        0,
-        360
-      );
-
-      string memory planetSVG = getOrbitSVG(planet);
-      renderSvg = string.concat(renderSvg, planetSVG);
-    }
-
-    renderSvg = string.concat(renderSvg, "</svg>");
-
-    return renderSvg;
   }
 
   function render(uint256 _tokenId) public pure returns (string memory) {
-    return getSVG(_tokenId);
+    Kaleidoscope memory kaleidescope = kaleidoscopeForTokenId(_tokenId);
+    ColorPalette memory palette = colorPaletteForKaleidescope(kaleidescope);
+
+    AnimatedCircle memory circle = circleAtIndexForKaleidescope(kaleidescope, palette, 0);
+    string memory circleSVG = getSVGForCircle(circle);
+
+    string memory svg = string.concat(
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ',
+      utils.uint2str(SIZE),
+      " ",
+      utils.uint2str(SIZE),
+      '">',
+      circleSVG,
+      "</svg>"
+    );
+
+    return svg;
   }
 }
--- backend/contracts/Utilities.sol
+++ backend/contracts/Utilities.sol
@@ -3,6 +3,12 @@ pragma solidity ^0.8.12;
 
 // Core utils used extensively to format CSS and numbers.
 library utils {
+  struct HSL {
+    uint256 h;
+    uint256 s;
+    uint256 l;
+  }
+
   function random(string memory input) internal pure returns (uint256) {
     return uint256(keccak256(abi.encodePacked(input)));
   }
@@ -14,11 +20,47 @@ library utils {
     uint256 upper
   ) internal pure returns (uint256) {
     uint256 rand = random(string(abi.encodePacked(keyPrefix, uint2str(tokenId))));
-    return (rand % (upper - lower)) + lower;
+    return (rand % (upper - lower + 1)) + lower;
   }
 
   function min(uint256 a, uint256 b) internal pure returns (uint256) {
     return a < b ? a : b;
+  }
+
+  function generateColors(uint256 _hue, uint256 _numColors) public pure returns (HSL[] memory) {
+    HSL[] memory colors = new HSL[](_numColors);
+
+    for (uint256 i = 0; i < _numColors; i++) {
+      colors[i] = HSL(_hue, 100 - ((i * 50) / _numColors), 70 - ((i * 30) / _numColors));
+    }
+
+    return colors;
+  }
+
+  function getHueName(uint256 _hue) public pure returns (string memory) {
+    _hue = _hue % 360;
+
+    string[12] memory colors = [
+      "Red",
+      "Orange",
+      "Yellow",
+      "Chartreuse",
+      "Green",
+      "Spring green",
+      "Turquoise",
+      "Teal",
+      "Blue",
+      "Violet",
+      "Magenta",
+      "Rose"
+    ];
+
+    uint256 colorIndex = (_hue / 30) % colors.length;
+    return colors[colorIndex];
+  }
+
+  function getHslString(HSL memory _hsl) public pure returns (string memory) {
+    return string(abi.encodePacked("hsl(", uint2str(_hsl.h), ",", uint2str(_hsl.s), "%,", uint2str(_hsl.l), "%)"));
   }
 
   // converts an unsigned integer to a string

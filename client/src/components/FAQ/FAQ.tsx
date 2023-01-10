610fe24fa3cc2e2d0144317171abd2047350f124
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2023-01-11 01:08:58 +0200

    Add copy

--- client/src/components/FAQ/FAQ.tsx
+++ client/src/components/FAQ/FAQ.tsx
@@ -12,21 +12,26 @@ export function FAQ({ etherscanBaseURL, deployAddress, playGeneralClick }: IFAQ)
           <p className="font-medium text-xl pb-4 text-gray-100">FAQ</p>
           <div className=" ">
             <div className="block bg-zinc-800 px-3 py-2 rounded-lg w-100 text-sm text-gray-100 ">
-              <span>What are Solar Kaleidoscopes?</span>
+              <span>What is Kaleidoscopes?</span>
             </div>
             <p className="text-sm text-zinc-500 px-3 pt-3 pb-5" id="allowlist-anchor">
-              Solar Systems is a fully on-chain NFT collection which features procedurally generated planets orbiting
-              around a star. Each Solar System is unique and can be minted for the price of 0.01 ETH. The collection is
-              limited to 1,000 Solar Systems.
+              Kaleidoscopes is the second instalment of fully on-chain collectibles by the team that brought you{" "}
+              {/* TODO: Style properly */}
+              <a href="https://onchainsolar.systems" target="_blank" rel="noopener noreferrer">
+                Solar Systems
+              </a>
+              . This new collection features fully on-chain, procedurally generated, animated kaleidescopes. They are
+              available for a minting price of 0.01 ETH with a maximum supply of 1,000 on the Ethereum blockchain.
+              Wallets can mint up to 20 each.
             </p>
           </div>
 
           <div className="block bg-zinc-800 px-3 py-2 rounded-lg w-100 text-sm text-gray-100 ">
             <span>Features</span>
           </div>
-          <div className="grid grid-cols- md:grid-cols-2">
+          <div className="grid grid-cols- md:grid-cols-2 pb-5">
             <div className="text-sm text-zinc-500 px-3 pt-3 col-span-1">
-              Each Solar System is
+              Each Kaleidescope is
               <ul className="space-y-2 mt-2  list-disc list-inside ml-3">
                 <li>
                   <a
@@ -41,20 +46,56 @@ export function FAQ({ etherscanBaseURL, deployAddress, playGeneralClick }: IFAQ)
                   . This means that your NFT will exist for as long as the Ethereum blockchain is around.
                 </li>
                 <li>
-                  <span className="font-bold">Animated.</span> Planets orbit around a star which adds to a dynamic and
-                  lively viewing experience.
+                  <span className="font-bold">Animated.</span> Each Kaleidescope has a base pattern containing
+                  procedurally generated moving shapes repeated in a circle up to 20 times to produce a mezmerising
+                  animation with traits unique to that specific token.
                 </li>
               </ul>
             </div>
             <div className="text-sm text-zinc-500 px-3 pt-3 ">
               <ul className="space-y-2  md:mt-7  list-disc list-inside ml-3">
                 <li>
-                  <span className=" font-bold">Procedurally generated.</span> This means that the solar systems are
-                  generated using a set of rules or procedures, rather than being created manually or pre-designed. This
-                  makes each solar system fully unique.
+                  <span className=" font-bold">Procedurally generated.</span> Traits for each Kaleidescope are generated
+                  using a set of procedures to produce stunning visuals, rather than being created manually or
+                  pre-designed. This makes each Kaleidescope fully unique.
                 </li>
               </ul>
             </div>
+          </div>
+
+          <div className=" ">
+            <div className="block bg-zinc-800 px-3 py-2 rounded-lg w-100 text-sm text-gray-100 ">
+              <span>What is the mint schedule?</span>
+            </div>
+            <p className="text-sm text-zinc-500 px-3 pt-3 pb-5" id="allowlist-anchor">
+              {/* TODO: Add final dates and times */}
+              Allowlisted addresses can mint from block 16399100 (approximately 4pm UTC on Friday, 13 January 2023). The
+              allowlist mint will last 900 blocks (approximately 3 hours) after which the public mint will be open and
+              anyone can mint.
+            </p>
+          </div>
+
+          <div className=" ">
+            <div className="block bg-zinc-800 px-3 py-2 rounded-lg w-100 text-sm text-gray-100 ">
+              <span>Who is on the allowlist?</span>
+            </div>
+            <p className="text-sm text-zinc-500 px-3 pt-3 pb-5" id="allowlist-anchor">
+              The allowlist consists of holders of Solar Systems collection as of 1:50pm UTC on Tuesday, 10 January
+              2023. The list of eligible addresses can be found {/* TODO: Add allowlist URL */}
+              <a href="http://" target="_blank" rel="noopener noreferrer">
+                here
+              </a>
+              .
+            </p>
+          </div>
+
+          <div className=" ">
+            <div className="block bg-zinc-800 px-3 py-2 rounded-lg w-100 text-sm text-gray-100 ">
+              <span>Roadmap?</span>
+            </div>
+            <p className="text-sm text-zinc-500 px-3 pt-3 pb-5" id="allowlist-anchor">
+              There is no roadmap.
+            </p>
           </div>
         </div>
       </div>

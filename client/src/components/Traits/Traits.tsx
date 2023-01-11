3c1fc1da8ebd9e2d34689667a0d34080f853c285
lbowles <lspbowles@gmail.com>
2023-01-11 17:33:05 +0200

    Updated traits

--- client/src/components/Traits/Traits.tsx
+++ client/src/components/Traits/Traits.tsx
@@ -1,6 +1,9 @@
-import maxSaturation from "../.././img/maxSaturation.svg"
+import maxSaturation from "../.././img/maxSaturation.png"
 import noReflections from "../.././img/noReflections.svg"
 import inputShapes from "../.././img/inputShapes.svg"
+import halo from "../.././img/halo.svg"
+import animation from "../.././img/animation.svg"
+import andMore from "../.././img/andMore.svg"
 
 export function Traits() {
   return (
@@ -8,23 +11,41 @@ export function Traits() {
       <div className="block  bg-zinc-900 border border-zinc-800 rounded-lg w-[800px]">
         <div className="p-5">
           <p className="font- text-xl pb-4 text-gray-100">Traits</p>
-          <div className="grid  gap-4 grid-cols-1 sm:grid-cols-3  ">
+          <div className="grid  gap-4 grid-cols-1 sm:grid-cols-2  ">
             <div>
               <div className=" bg-zinc-800 px-3 py-4 rounded-lg w-100 text-sm text-gray-100 flex  items-center ">
                 <img src={noReflections} alt="Number Of Reflections" className="h-5 mr-3"></img>
-                <span>Number Of Reflections</span>
+                <span>Number of reflections</span>
               </div>
             </div>
             <div>
               <div className=" bg-zinc-800 px-3 py-4 rounded-lg w-100 text-sm text-gray-100 flex  items-center min-h-20">
                 <img src={maxSaturation} alt="Max Saturation" className="h-5 mr-3"></img>
-                <span>Colour/Gradients</span>
+                <span>Color palette</span>
               </div>
             </div>
             <div>
               <div className=" bg-zinc-800 px-3 py-4 rounded-lg w-100 text-sm text-gray-100 flex  items-center">
                 <img src={inputShapes} alt="Number Of Reflections" className="h-5 mr-3"></img>
-                <span>Input Shape Complexity</span>
+                <span>Number of shapes</span>
+              </div>
+            </div>
+            <div>
+              <div className=" bg-zinc-800 px-3 py-4 rounded-lg w-100 text-sm text-gray-100 flex  items-center">
+                <img src={halo} alt="Number Of Reflections" className="h-5 mr-3"></img>
+                <span>Halo effect for first 50 tokens</span>
+              </div>
+            </div>
+            <div>
+              <div className=" bg-zinc-800 px-3 py-4 rounded-lg w-100 text-sm text-gray-100 flex  items-center">
+                <img src={animation} alt="Number Of Reflections" className="h-4 mr-2"></img>
+                <span>Unique Animations</span>
+              </div>
+            </div>
+            <div>
+              <div className=" bg-zinc-800 px-3 py-4 rounded-lg w-100 text-sm text-gray-100 flex  items-center">
+                <img src={andMore} alt="Number Of Reflections" className="w-[20px] mr-2"></img>
+                <span>And more...</span>
               </div>
             </div>
           </div>
